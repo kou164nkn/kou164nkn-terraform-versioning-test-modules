fd127928cb1fa4554616247255ba81f40e49aa40
Hiroshi Nakano <kou164nkn@gmail.com>
2022-03-11 01:20:17 +0900

    Added modules2

---
+++ modules2/variables.tf
@@ -0,0 +1,22 @@
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC."
+  type        = string
+  default     = "10.0.0.0/8"
+}
+
+variable "vpc_name" {
+  description = "The name for the VPC."
+  type        = string
+}
+
+variable "enable_dns_support" {
+  description = "The flag to enable dns support"
+  type        = bool
+  default     = true
+}
+
+variable "tags" {
+  description = "A map of tags to add to VPC."
+  type        = map(string)
+  default     = {}
+}
